b0b83b73f55036d47514b274315bc7bc772ae6fd
liguoqi <guo-qingl@microfocus.com>
2022-08-22 22:24:41 +0800

    Annotation @JsonAnyGetter usage

--- src/main/java/com/bing/annotations/bean/ExtendableBean.java
+++ src/main/java/com/bing/annotations/bean/ExtendableBean.java
@@ -20,6 +20,7 @@
 package com.bing.annotations.bean;
 
 import com.fasterxml.jackson.annotation.JsonAnyGetter;
+import java.util.HashMap;
 import java.util.Map;
 
 /**
@@ -31,9 +32,22 @@ public class ExtendableBean {
     public String name;
     private Map<String, String> properties;
 
+    public ExtendableBean(String name) {
+        this.name = name;
+    }
+
+    public ExtendableBean() {
+    }
+
     @JsonAnyGetter
     public Map<String, String> getProperties() {
         return properties;
     }
 
+    public void add(String key, String value) {
+        if(properties==null){
+            properties = new HashMap<>();
+        }
+        properties.put(key,value);
+    }
 }
--- src/test/java/com/bing/annotations/bean/ExtendableBeanTest.java
+++ src/test/java/com/bing/annotations/bean/ExtendableBeanTest.java
@@ -1,6 +1,14 @@
 package com.bing.annotations.bean;
 
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.containsString;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 /**
  * @author Bing
  * @summary
@@ -9,8 +17,17 @@ package com.bing.annotations.bean;
 class ExtendableBeanTest {
 
 
-    public void testJsonAnyGetter(){
+    @Test
+    public void testJsonAnyGetter() throws JsonProcessingException {
+        ExtendableBean bean = new ExtendableBean("My bean");
+        bean.add("attr1", "val1");
+        bean.add("attr2", "val2");
 
+        String result = new ObjectMapper().writeValueAsString(bean);
+
+        System.out.println(result);
+        assertThat(result, containsString("attr1"));
+        assertThat(result, containsString("val1"));
     }
 
 }
